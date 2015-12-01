99ae4383e1bb33d8c0a84649834ad7f5f4da362b
James Traver <jtraverjr@gmail.com>
2015-12-01 16:11:03 -0600

    its workig, its worrrrking

--- controllers/recommendations.rb
+++ controllers/recommendations.rb
@@ -1,5 +1,23 @@
 class RecommendationsController < ApplicationController
   get '/' do
+    @recommendations = Recommendation.all
+    erb :read_recommendation
+  end
+
+  get '/create' do
+    erb :create_recommendation
+  end
+
+  post '/create' do
+    p params
+
+    @rec = Recommendation.new
+    @rec.contributor = params[:contributor]
+    @rec.place_name = params[:name]
+    @rec.type_of_place = params[:type_of_place]
+    @rec.notes = params[:notes]
+    @rec.save
+
     erb :index
   end
 
@@ -7,4 +25,10 @@ class RecommendationsController < ApplicationController
     # select * from recommendations;
     Recommendation.all.to_json
   end
+
+  get '/api/:id' do
+    p params
+    return Recommendation.find(params[:id]).to_json
+  end
+
 end
